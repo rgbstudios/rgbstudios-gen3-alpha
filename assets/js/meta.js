67efdf5a5463bb9649e86959ddfd045c61681105
RGB Studios <justingolden21@gmail.com>
2018-08-27 21:23:56 -0700

    added dynamic themes

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,8 +1,16 @@
 let colors = ['#933', '#393', '#339'];
-let themeColor = colors[Math.floor(Math.random()*colors.length)];
+let colorNames = ['red-select', 'green-select', 'blue-select'];
+
+let idx = Math.floor(Math.random()*colors.length);
+
+let themeColor = colors[idx];
+let cssClass = colorNames[idx];
 
 $('#meta1').prop('content',themeColor);
 $('#meta2').prop('content',themeColor);
 $('#meta3').prop('content',themeColor);
 
-$('::selection').prop('color', themeColor);
+$( document ).ready(function() {
+  $('*').addClass(cssClass);
+});
+
