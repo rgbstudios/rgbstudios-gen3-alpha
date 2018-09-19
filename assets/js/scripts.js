d230ce189ea510d831a2bf33766bc755eb960d1a
Justin Golden <justingolden21@gmail.com>
2018-09-19 14:02:39 -0700

    update icons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,7 @@
 window.onload = function() {
 
-	//meta.js
+	//meta.js colors
 	$('*').addClass(cssClass);
-
-
 
   let url = new URL(window.location.href);
   console.log(url);
@@ -15,20 +13,17 @@ window.onload = function() {
 
 
   //https://stackoverflow.com/questions/12115833/adding-a-slide-effect-to-bootstrap-dropdown
-  // Add slideDown animation to Bootstrap dropdown when expanding.
   $('.dropdown').on('show.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
   });
-
-  // Add slideUp animation to Bootstrap dropdown when collapsing.
   $('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
 
+  //search
   $('#navSearch').on('keyup', doSearch);
   $('#searchForm').on('submit', function(e) { //hit enter in input or click button
     e.preventDefault();
-    console.log('searchin...');
     $('#navRgbText').click();
     window.open('../../index.html/?q=' + $('#navSearch').val(), '_self');
   });
